89978b5c425014cbe05ef7fd0228a6eb19f79dc4
ImHenriqueCampello <henrique.campello@ges.inatel.br>
2025-08-19 20:21:55 -0300

    Funcionalidade adicionada, porém ela está bugada!

--- src/project_02/main.py
+++ src/project_02/main.py
@@ -1,12 +1,14 @@
 import requests
+from datetime import datetime
 
 def get_github_status():
-    response = requests.get("https://api.github.com")
+    response = requests.get("https://api.github.com"/)
     return {
         "status_code": response.status_code,
-        "response_time_ms": response.elapsed.total_seconds() * 1000
+        "response_time_ms": response.elapsed.total_seconds() * 1000,
+        "checked_at": datetime.now().strfime("%Y-%m-%d %H:%M:%S")
     }
 
-if __name__ == "__main__":
+if name == "main":
     status = get_github_status()
-    print(f"GitHub API status: {status['status_code']} - Response time: {status['response_time_ms']:.2f} ms")
+    print(f"[{status['checked_at']}] GitHub API status: {status['status_code']} - Response time: {status['response_time_ms']:.2f} ms")
