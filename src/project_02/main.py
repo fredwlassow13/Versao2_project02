cfd4cf5f554afa6f6e212e710e97811e66eb0731
Fredwlassow <Fredwlassow13@gmail.com>
2025-08-19 11:31:38 -0300

    Primeiro commit a partir do backup

---
+++ src/project_02/main.py
@@ -0,0 +1,8 @@
+import requests
+
+def get_github_status():
+    response = requests.get("https://api.github.com")
+    return response.status_code
+
+if __name__ == "__main__":
+    print("GitHub API status:", get_github_status())
